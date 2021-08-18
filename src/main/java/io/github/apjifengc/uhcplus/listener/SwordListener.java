bce7cd78f7665a8c215a2d1035c9687cec419284
Milk Ory <youranpublic@gmail.com>
2021-08-18 21:44:12 +0800

    🐞 Fix

--- src/main/java/io/github/apjifengc/uhcplus/listener/SwordListener.java
+++ src/main/java/io/github/apjifengc/uhcplus/listener/SwordListener.java
@@ -5,7 +5,6 @@ import io.github.apjifengc.uhcplus.UhcPlus;
 import io.github.apjifengc.uhcplus.util.ItemUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.Sound;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -33,17 +32,18 @@ public class SwordListener implements Listener {
     @EventHandler
     public void onPlayerHit(EntityDamageByEntityEvent event) {
         if (event.getDamager() instanceof Player) {
-            Player damager = (Player) event.getDamager();
-            Player damagee = (Player) event.getEntity();
-            if (taskMap.containsKey(damager)) return;
-            if (ItemUtil.isSimilar(Config.SWORD, damager.getInventory().getItemInMainHand())) {
+            var from = event.getDamager();
+            if (!(from instanceof Player)) return;
+            var to = event.getEntity();
+            if (taskMap.containsKey(from)) return;
+            if (ItemUtil.isSimilar(Config.SWORD, from.getInventory().getItemInMainHand())) {
                 event.setDamage(event.getDamage() + Config.addDamage);
-                damager.getWorld().playSound(event.getEntity().getLocation(), Sound.ENTITY_GENERIC_EXPLODE, 1.0F, 1.0F);
-                damagee.addPotionEffect(new PotionEffect(PotionEffectType.HARM, 1,0));
-                taskMap.put(damager, new BukkitRunnable() {
+                from.getWorld().playSound(event.getEntity().getLocation(), Sound.ENTITY_GENERIC_EXPLODE, 1.0F, 1.0F);
+                to.addPotionEffect(new PotionEffect(PotionEffectType.HARM, 1,0));
+                taskMap.put(from, new BukkitRunnable() {
                     @Override
                     public void run() {
-                        taskMap.remove(damager);
+                        taskMap.remove(from);
                     }
                 }.runTaskLater(plugin, (long) (Config.cd * 20L)));
             }
